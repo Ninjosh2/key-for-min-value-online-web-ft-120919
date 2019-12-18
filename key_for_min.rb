4f4e981181d385bd5fae0d46aa5e514a0f311e77
Joshua Sanders <ninjosh2@gmail.com>
2019-12-18 04:52:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,10 @@
 guitars = {:jericho => 150, :gibson => 190, :fender => 500}
 
-
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(guitars)
-  min_value > 150
+  min_value == 150
     lowest_key = nil
   end
 end
