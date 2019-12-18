b34d595f1f8fa8bfa97aa8ca13ffd3e211431e30
Joshua Sanders <ninjosh2@gmail.com>
2019-12-18 04:48:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,13 @@ guitars = {:jericho => 150, :gibson => 190, :fender => 500}
 
 def key_for_min_value(guitars)
   min_value = 150
-
+  lowest_key = nil
+  
+  name_hash.each do |key,value|
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
